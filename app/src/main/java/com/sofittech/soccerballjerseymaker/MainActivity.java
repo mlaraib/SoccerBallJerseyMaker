a6fdb560ce9729fb9da5c41aea9029656b0c5af7
M Laraib <iamlaraibkhan@gmail.com>
2016-11-04 11:30:14 +0500

    edittext issue resolved on MainActivity.java. It will moved now from edittext to another by clicking enter.

--- app/src/main/java/com/sofittech/soccerballjerseymaker/MainActivity.java
+++ app/src/main/java/com/sofittech/soccerballjerseymaker/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Selection;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,61 @@ public class MainActivity extends AppCompatActivity {
 
         name= (EditText) findViewById(R.id.username);
         number= (EditText) findViewById(R.id.shirtnumber);
+
+
+
+//Test 1
+//        name.setOnKeyListener(new View.OnKeyListener() {
+//
+//            @Override
+//            public boolean onKey(View v , int keyCode , KeyEvent event) {
+//
+//                EditText number = (EditText) findViewById(R.id.shirtnumber);
+//
+//                // TODO Auto-generated method stub
+//                if (keyCode == event.KEYCODE_ENTER) {
+//
+//                    Selection.setSelection((Editable) number.getText(), name.getSelectionStart());
+//                    number.requestFocus();
+//                }
+//
+//                return true;
+//            }
+//        });
+
+
+
+
+        name.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER))
+                {
+                    // Perform action on Enter key press
+                    name.clearFocus();
+                    number.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        number.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER))
+                {
+                    // Perform action on Enter key press
+                    // check for username - password correctness here
+                    return true;
+                }
+                return false;
+            }
+        });
 
 
         next= (Button) findViewById(R.id.btnNext);
